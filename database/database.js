15552bd20d2ccceaef01c6d5b2b955e23f0995b7
Tuomas Vanhala <tuomas.j.vanhala@aalto.fi>
2020-12-05 11:31:13 +0200

    Added database connection pool and query execution.

---
+++ database/database.js
@@ -0,0 +1,18 @@
+import { Pool } from "../deps.js";
+import { config } from "../config/config.js";
+
+const CONCURRENT_CONNECTIONS = 5;
+
+const executeQuery = async(query, ...args) => {
+  const client = new Pool(config.database, CONCURRENT_CONNECTIONS);
+  try {
+    await client.connect();
+    return await client.query(query, ...args);
+  } catch (e) {
+    console.log(e);
+  } finally {
+    await client.end();
+  }
+}
+
+export { executeQuery };
