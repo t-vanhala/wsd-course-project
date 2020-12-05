5393d53d1985da5b52445bb23510fff70486b643
Tuomas Vanhala <tuomas.j.vanhala@aalto.fi>
2020-12-05 11:27:27 +0200

    Added first middlewares.

---
+++ middlewares/middlewares.js
@@ -0,0 +1,16 @@
+const errorMiddleware = async(context, next) => {
+  try {
+    await next();
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+const requestTimingMiddleware = async({ request }, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${request.method} ${request.url.pathname} - ${ms} ms`);
+}
+
+export { errorMiddleware, requestTimingMiddleware };
