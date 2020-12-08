d0091825aa801ff7220df5a274a66d4c713e8870
Tuomas Vanhala <tuomas.j.vanhala@aalto.fi>
2020-12-08 13:57:15 +0200

    Removed unneeded stuff from reporting page and improved it.

--- static/code.js
+++ static/code.js
@@ -1,8 +1,1 @@
-const selectFunction = () => {
-    // Check which one report is chosen
-    if (document.querySelector('#morning').checked) {
-        return true;
-    } else {
-        return false;
-    }
-}
+test
